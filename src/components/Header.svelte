49ebaa226ee0b6c3b24b81450ccf73ad66b7437a
Jeremiah Franco <jeremiahfranco3@gmail.com>
2025-03-13 14:15:07 -0600

    Added Svelte

---
+++ src/components/Header.svelte
@@ -0,0 +1,12 @@
+<header id="homeHeader">
+    <h2><a href="#">Movie Website</a></h2>
+    <nav>
+        <ul>
+            <li>
+                <a href="/user/login.html">Login</a>
+                <a href="user/signup.html">Sign up</a>
+                <a href="user/dashboard.html">Dashboard</a>
+            </li>
+        </ul>
+    </nav>
+  </header>
---
+++ src/main.js
@@ -0,0 +1,19 @@
+import { mount } from 'svelte'
+import './styles/base.css'
+import App from './App.svelte'
+import Header from './components/Header.svelte'
+import Footer from './components/Footer.svelte'
+
+const app = mount(App, {
+  target: document.getElementById('app'),
+})
+
+const HeaderComponent = mount(Header, {
+  target: document.getElementById('header'),
+})
+
+const FooterComponent = mount(Footer, {
+  target: document.getElementById('footer'),
+})
+
+export default app
