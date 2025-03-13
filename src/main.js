4cd3fbd45e41c892de8ba44cc157276ada0e5567
Jeremiah Franco <jeremiahfranco3@gmail.com>
2025-03-13 14:19:58 -0600

    Working on main.js

--- src/main.js
+++ src/main.js
@@ -4,16 +4,17 @@ import App from './App.svelte'
 import Header from './components/Header.svelte'
 import Footer from './components/Footer.svelte'
 
-const app = mount(App, {
+// const app = 
+mount(App, {
   target: document.getElementById('app'),
 })
 
-const HeaderComponent = mount(Header, {
+mount(Header, {
   target: document.getElementById('header'),
 })
 
-const FooterComponent = mount(Footer, {
+mount(Footer, {
   target: document.getElementById('footer'),
 })
 
-export default app
+// export default app
