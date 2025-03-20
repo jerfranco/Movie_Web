33df0fd134776e31f35a0980690918d1421878ac
TeaJay13 <131283247+TeaJay13@users.noreply.github.com>
2025-03-20 14:23:05 -0600

    search implementation

--- src/App.svelte
+++ src/App.svelte
@@ -1,6 +1,17 @@
 <script>
   import { onMount } from "svelte";
   import PopularMovies from "./components/PopularMovies.svelte";
+  import Search from './components/Search.svelte';
+  import { writable } from 'svelte/store';
+
+    // Create a writable store for the search query
+    export let searchQuery = writable("");
+    let searchComponent;
+
+    async function handleSearch() {
+        searchQuery.set(document.getElementById('search').value);
+        await searchComponent.fetchMovies();
+    }
 </script>
 
 <main>
@@ -12,16 +23,14 @@
     <a href="#"><img src="src/images/sci_fi-genre.svg" alt="Sci-Fi Genre" /></a>
   </div>
   <div id="searchMovie">
-    <input
-      type="text"
-      id="search"
-      name="search"
-      placeholder="Enter Movie Name Here"
-    />
-    <button id="searchButton"><img src="src/images/searchIcon.png" /></button>
-    <!-- <img src="images/searchIcon.png" alt="test"> -->
-  </div>
+    <input type="text" id="search" name="search" placeholder="Enter Movie Name Here" bind:value={$searchQuery}>
+    <button id="searchButton" on:click={handleSearch}>
+        <img src="src/images/searchIcon.png">
+    </button>
+</div>
   <div id="popMovies">
+    <Search bind:this={searchComponent} {searchQuery} />
     <PopularMovies />
+  
   </div>
 </main>
---
+++ src/components/Search.svelte
@@ -0,0 +1,60 @@
+<script>
+  import { onMount } from "svelte";
+  import { get } from "svelte/store";
+
+  export let searchQuery;
+
+  let movies = [];
+
+  export async function fetchMovies() {
+    const search = get(searchQuery);
+    if (!search) return;
+
+    const response = await fetch(
+      `https://www.omdbapi.com/?apikey=93c9c418&s=${search}`
+    );
+    const data = await response.json();
+    console.log(data);
+    movies = data.Search;
+    console.log(movies);
+  }
+</script>
+
+<div id="popMovies">
+  <ul>
+    {#each movies as movie}
+      <li>
+        <img src={movie.Poster} alt={movie.Title} />
+        <h3>{movie.Title}</h3>
+      </li>
+    {/each}
+  </ul>
+</div>
+
+<style>
+  #popMovies {
+    text-align: center;
+    padding-top: 3%;
+  }
+
+  #popMovies ul {
+    padding: 0;
+    width: 65%;
+    display: grid;
+    grid-template-columns: repeat(3, 1fr);
+    margin-left: auto;
+    margin-right: auto;
+  }
+
+  #popMovies ul li {
+    margin: 1rem;
+
+    padding: 0.5rem;
+  }
+
+  #popMovies img {
+    max-width: 100%;
+    height: auto;
+    border-radius: 10px;
+  }
+</style>
