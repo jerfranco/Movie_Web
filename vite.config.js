e42911de116169c696c820d175215915fe2f2a6c
Jeremiah Franco <jeremiahfranco3@gmail.com>
2025-04-03 13:41:22 -0600

    Fixing dist

--- vite.config.js
+++ vite.config.js
@@ -9,7 +9,7 @@ export default defineConfig({
   plugins: [svelte()],
   root: "src",
   build: {
-    outDir: "./dist",
+    outDir: "../dist",
     rollupOptions: {
       input: {
         main: resolve(__dirname, "src/index.html"),
